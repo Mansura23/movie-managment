22d741b285fc44a548c48ea5c5ac81b1a62db9f1
Mansura <badalovamansura40@gmail.com>
2025-03-28 13:07:43 +0400

    Added 1 and 2 step

--- src/main/java/az/moviemanagement/Main.java
+++
@@ -1,7 +0,0 @@
-package az.moviemanagement;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/az/moviemanagement/MovieApp.java
@@ -0,0 +1,19 @@
+package az.moviemanagement;
+
+import java.util.Scanner;
+
+public class MovieApp {
+    private Movie[] movies =new Movie[3] ;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextInt()){
+            int input = sc.nextInt();
+            if(input == 1){
+
+
+            }
+        }
+    }
+
+}
